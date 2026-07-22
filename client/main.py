3b6632f08ef55f86d7c3f9d3a21718a881d2d249
Kaushik Venkatesh <kaushik@Kaushiks-MacBook-Pro.local>
2026-07-22 17:04:13 -0400

    implement main scripts

---
+++ client/main.py
@@ -0,0 +1,41 @@
+from scapy.all import sniff
+import struct
+import socket
+
+SERVER = "172.16.109.131"
+PORT = 5000
+IFACE = "en0"
+
+
+def detect(pkt, client):
+    try:
+        raw_pkt = bytes(pkt)
+        pkt_len = struct.pack("!I", len(raw_pkt))
+        client.sendall(pkt_len + raw_pkt)
+    except BrokenPipeError:
+        print("VM disconnected — stopping capture")
+        raise SystemExit
+    except Exception as e:
+        print("ERROR:", e)
+
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((SERVER, PORT))
+
+    try:
+        sniff(
+            iface=IFACE,
+            prn=lambda pkt: detect(pkt, client),
+            store=False
+        )
+    except KeyboardInterrupt:
+        print("Stopping capture...")
+    except BrokenPipeError:
+        print("Session terminated on VM")
+    finally:
+        client.close()
+
+
+if __name__ == "__main__":
+    main()
